95410892ef9cdd976b721fbbf0ecaf6fd68c7246
Mquan280810 <hoangminhquan28082010@gmail.com>
2025-06-22 22:05:57 +0700

    Add files via upload

---
+++ project.py
@@ -0,0 +1,7 @@
+import streamlit as st
+
+st.title("Gioi thieu ban than")
+st.write("Ho va Ten: Hoang Nguyen Minh Quan")
+st.write("Ngay thanh nam sinh: 28/8/2010")
+st.write("Mon hoc yeu thich: Mon Tieng Anh")
+st.write("So thich: Danh cau, Danh dan piano")
